70a781a102f350f1a2f1f3615e315dc4e23c1566
Tamas <tbudavari@gmail.com>
2024-03-03 22:50:06 +0100

    quantity előtti shop

---
+++ webshopdata.js
@@ -0,0 +1,15 @@
+const products=[
+    {id:'id1',
+    img:'<img src="/img/img1.jpg">',
+    name:'Black and Gray Athletic Cotton Socks - 6 Pairs'
+    },
+    {id:'id2',
+    img:'<img src="/img/img2.jpg">',
+    name:'Intermediate Basketball'
+    },
+    {id:'id3',
+    img:'<img src="/img/img3.jpg">',
+    name:'Adult man T-shirts blue'
+    }
+    ];
+
