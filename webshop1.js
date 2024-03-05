b82dbb086e32cbe3dd121ca1a2e80db6ba445044
Tamas <tbudavari@gmail.com>
2024-03-05 22:34:13 +0100

    cart quantity-vel, weblapra nincs kiírva

--- webshop1.js
+++ webshop1.js
@@ -6,28 +6,33 @@ products.forEach((value, index) =>{
         <div class="img-div">${arrayElement.img}</div>
         <div>${arrayElement.id}</div>
         <div class="name-div">${arrayElement.name}</div>
-        <button data-prod="${arrayElement.id}">Add to Cart</button>
+        <button class="js-product-div" data-prod="${arrayElement.id}">Add to Cart</button>
         </div>
         `;
     });
     document.querySelector('.js-product-div').innerHTML=html;
     document.querySelectorAll('button').forEach((button)=>{
-            button.addEventListener('click',()=>{
-                const productId=button.dataset.prod;
+        button.addEventListener('click',()=>{
+            const productId=button.dataset.prod;
+        
+            let matchingItem;
             
-                let matchingItem;
-                cart.forEach((value, index)=>{
-                    if (productId===value.productId){
-                        value.quantity+=1;
-                        console.log(value)
-                    }
-                });
+            cart.forEach((item)=>{
+                if (productId===item.productId){
+                    matchingItem=item;
+                }
+            });
+
+            if (matchingItem){
+                matchingItem.quantity+=1;
+            }else{
                 cart.push(
                     {productId:productId,
                     quantity:1
                     });
-                    console.log(cart); 
-                    document.querySelector('.js-header').innerHTML=cart.length;
-            });    
-        });
-    
+            }
+            
+                console.log(cart); 
+                document.querySelector('.js-header').innerHTML=matchingItem.quantity;
+        });    
+    });
